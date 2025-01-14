68791aa06466a3a1796179acba792367ee962c7c
Samuel Vilas Boas Souza <113648081+samlvbs@users.noreply.github.com>
2025-01-14 11:49:06 -0300

    Alteracao no env para deploy

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,14 +1,12 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: process.env.FIREBASE_API_KEY as string,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
-    projectId: process.env.FIREBASE_PROJECT_ID as string,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
-    appId: process.env.FIREBASE_APP_ID as string
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET ,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ,
+    appId: process.env.FIREBASE_APP_ID
   }
 };
